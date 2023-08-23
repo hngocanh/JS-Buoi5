6bf8e2be2684f38111cd10140f5eb7c85e182770
Hoang-Ngoc-Anh <hoangngocanh288@gmail.com>
2023-08-23 22:55:20 +0700

    update

--- Baitap/js/BaitapBuoi5.js
+++ Baitap/js/BaitapBuoi5.js
@@ -47,7 +47,7 @@ function tinhDiemTongKet() {
     var diemUuTien = tinhDiemUuTien();
     var diemTongKet = 0;
     var diemChuan = +document.getElementById("diemChuan").value;
-    
+
     var diemTongKet = diem1 + diem2 + diem3 + diemUuTien;
 
     if (diem1 === 0 || diem2 === 0 || diem3 === 0) {
@@ -59,3 +59,77 @@ function tinhDiemTongKet() {
     }
 }
 
+/*********Tính Tiền Điện */
+
+function tinhTienDien() {
+    var soKw = +document.querySelector('#kW').value;
+
+    var tienDien = 0;
+
+    if (soKw < 0) {
+        alert('Mời nhập số kW hợp lệ')
+    } else if (soKw === 0) {
+        alert('So kW không thể bằng 0')
+    } else if (soKw > 0 && soKw <= 50) {
+        tienDien = 50 * 500;
+    } else if (soKw > 50 && soKw <= 100) {
+        tienDien = 50 * 500 + ((soKw - 50) * 650);
+    } else if (soKw > 100 && soKw <= 200) {
+        tienDien = 50 * 500 + 50 * 650 + ((soKw - 100) * 850);
+    } else if (soKw > 200 && soKw <= 350) {
+        tienDien = 50 * 500 + 50 * 650 + 100 * 850 + ((soKw - 200) * 1100);
+    } else {
+        tienDien = 50 * 500 + 50 * 650 + 100 * 850 + 150 * 1100 + ((soKw - 350) * 1300);
+    }
+
+    document.querySelector('#tienDien').textContent = tienDien;
+}
+
+/*********Tính Tiền Cáp***********/
+
+// 
+
+// const LOAI_KHACH_HANG = document.querySelector('#loaiKH').value;
+// const SO_KET_NOI = +document.querySelector('#soKetNoi').value;
+// const SO_KENH_CAO_CAP = +document.querySelector('#soKenhCaoCap').value;
+
+// function tinhTienCaNhan() {
+//     var phiHoaDon = 4.5;
+//     var basicServices = 20.5;
+//     var kenhCaoCap = 7.5;
+//     var tienCaNhan = 0;
+
+//     tienCaNhan = phiHoaDon + basicServices + kenhCaoCap * SO_KENH_CAO_CAP;
+//     return tienCaNhan;
+// }
+
+// function tinhTienDoanhNghiep() {
+//     var phiHoaDon = 15;
+//     var basicServices = 0;
+//     var kenhCaoCap = 50;
+
+//     var tienDoanhNghiep = 0;
+
+//     if (SO_KET_NOI <= 10) {
+//         basicServices = 75 * 10;
+//     } else if (SO_KET_NOI > 10) {
+//         basicServices = 75 * 10 + (SO_KET_NOI - 10) * 5;
+//     }
+
+//     tienDoanhNghiep = phiHoaDon + basicServices + kenhCaoCap * SO_KENH_CAO_CAP;
+//     return tienDoanhNghiep;
+// }
+
+// function tinhTienCap() {
+//     var tien = 0;
+//     switch (LOAI_KHACH_HANG) {
+//         case 'caNhan':
+//             tien = tinhTienCaNhan();
+//             break;
+//         case 'doanhNghiep':
+//             tien = tinhTienDoanhNghiep();
+//             break;
+//     }
+
+//     document.querySelector('#cableCost').textContent = tien;
+// }
